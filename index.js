d4ce8420cfeba8127f8c0e1034ba2a74744b4393
Lawzenn <35649900+Lawzenn@users.noreply.github.com>
2018-01-21 08:01:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ bot.on("ready", function() {
     console.log("Le bot a bien été connecté")
 });
 
-bot.login("NDA0NDM4MTg2MTY5NzI5MDI0.DUV34g.hQyC6ebWCj5Zp08YFgWApicH3XA");
+bot.login(process.env.TOKEN);
