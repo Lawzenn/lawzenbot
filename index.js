7c82c5084821aede86f639a88221d6cc72b5966b
Lawzenn <35649900+Lawzenn@users.noreply.github.com>
2018-01-21 10:49:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const Discord = require("discord.js");
+const bot = new Discord.Client();
 
-var bot = new Discord.Client();
+var prefix = ("!")
 
 bot.on("ready", function() {
     bot.user.setGame("ByLawzenn, !help")
-    console.log("Le bot a bien été connecté")
-
+    console.log("Connected");
 });
 
 bot.login("NDA0NDM4MTg2MTY5NzI5MDI0.DUV34g.hQyC6ebWCj5Zp08YFgWApicH3XA");
+
+
+bot.on('message', message => {
+  if (message.content === 'help') {
+    message.reply('L'aide est en devellopement' )
+  }
+})
