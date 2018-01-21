80844c286903b1738f003c51cc10a1f21e39fc9a
Lawzenn <35649900+Lawzenn@users.noreply.github.com>
2018-01-21 09:20:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const Discord = require("discord.js");
-
-var bot = new Discord.Client();
+const Discord = require('discord.js')
+const bot = new Discord.Client()
 
 bot.on("ready", function() {
     bot.user.setGame("ByLawzenn, !help")
     console.log("Le bot a bien été connecté")
-});
+
+    
+bot.on('message', message => {
+    if (message.content === '!ping') {
+      message.reply('pong !')
+    }
+  }
+
+})
+
 
 bot.login("NDA0NDM4MTg2MTY5NzI5MDI0.DUV34g.hQyC6ebWCj5Zp08YFgWApicH3XA");
