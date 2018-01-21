771fe3078c45f4e6bfba428040c432843606b12a
Lawzenn <35649900+Lawzenn@users.noreply.github.com>
2018-01-21 09:38:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ bot.on("ready", function() {
     bot.user.setGame("ByLawzenn, !help")
     console.log("Le bot a bien été connecté")
 
-    
-bot.on('message', message => {
-    if (message.content === '!ping') {
-      message.reply('pong !')
-    }
-  }
-
 });
 
 bot.login("NDA0NDM4MTg2MTY5NzI5MDI0.DUV34g.hQyC6ebWCj5Zp08YFgWApicH3XA");
