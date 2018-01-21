3871ed4e64970007f609d4f1e4f66cb6ade50189
Lawzenn <35649900+Lawzenn@users.noreply.github.com>
2018-01-21 11:03:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,3 @@ bot.on("ready", function() {
 });
 
 bot.login("NDA0NDM4MTg2MTY5NzI5MDI0.DUV34g.hQyC6ebWCj5Zp08YFgWApicH3XA");
-
-
-bot.on('message', message => {
-  if (message.content === prefix + "help"){
-      message.channel.sendMessage("Liste des commandes: \n -"help");
-  }
