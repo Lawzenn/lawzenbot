59c4817204be8c7ed6bf3125281b3568e7a4d5e9
Lawzenn <35649900+Lawzenn@users.noreply.github.com>
2018-01-21 08:00:50 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    bot.user.setGame("LawzennBot, !help")
+    console.log("Le bot a bien été connecté")
+});
+
+bot.login("NDA0NDM4MTg2MTY5NzI5MDI0.DUV34g.hQyC6ebWCj5Zp08YFgWApicH3XA");
