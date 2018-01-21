df7c26ce331f7812ab9e6e09cded6f23a4c4759f
Lawzenn <35649900+Lawzenn@users.noreply.github.com>
2018-01-21 09:12:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ var bot = new Discord.Client();
 bot.on("ready", function() {
     bot.user.setGame("ByLawzenn, !help")
     console.log("Le bot a bien été connecté")
-});
 
-bot.login("NDA0NDM4MTg2MTY5NzI5MDI0.DUV34g.hQyC6ebWCj5Zp08YFgWApicH3XA");
-
+    
 bot.on('message', message => {
-    if (message.content === 'ping') {
+    if (message.content === '!ping') {
       message.reply('pong !')
     }
   }
+
+});
+
+bot.login("NDA0NDM4MTg2MTY5NzI5MDI0.DUV34g.hQyC6ebWCj5Zp08YFgWApicH3XA");
